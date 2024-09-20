32ea046c096957debd71d3178b8a05933b9d816a
Stella Yu <stellashyou@gmail.com>
2024-09-20 23:44:41 +0900

    로고 추가

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -6,6 +6,9 @@ import { GeistMono } from "geist/font/mono";
 export const metadata: Metadata = {
   title: "Stella's dev blog",
   description: "스텔라의 기술 블로그",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 export default function RootLayout({
   children,
@@ -14,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={`${GeistMono.className}`}>
         <Navbar />
         <main className="container mx-auto lg:max-w-screen-md sm:max-w-screen-sm">
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
 import Link from "next/link";
 
 function Navbar() {
   return (
     <>
       <nav className="py-4 sticky top-0 left-0 right-0 bg-white bg-opacity-80 backdrop-blur-lg">
-        <div className="flex justify-end mx-auto lg:max-w-screen-md sm:max-w-screen-sm">
+        <div className="flex justify-between mx-auto lg:max-w-screen-md sm:max-w-screen-sm">
+          <div>
+            <Link href="/">
+              <Image src="/logo.png" width={24} height={24} alt="logo" />
+            </Link>
+          </div>
           <ul className="flex gap-6">
             <li>
               <Link href="/">Home</Link>
